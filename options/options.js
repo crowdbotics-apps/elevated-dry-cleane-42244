8e9f33f79639438a734b0e36b91aef404f9936af
Crowdbotics <team@crowdbotics.com>
2023-07-18 05:06:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "elevated-dry-cleane-42244",
+  url: "https://elevated-dry-cleane-42244.botics.co",
+  api: "https://elevated-dry-cleane-42244.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
